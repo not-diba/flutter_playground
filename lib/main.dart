3090b42cab895341e0db47a84a1bea58fa997aaf
Elvis Madiba <38106494+not-diba@users.noreply.github.com>
2022-12-26 21:56:07 +0300

    Stateless and Stateful widgets and FAB

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const Playground());
+
+class Playground extends StatelessWidget {
+  const Playground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Playground',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _pressedOrNot = "Button not pressed.";
+
+  void _changeText() {
+    setState(_getNewText);
+  }
+
+  void _getNewText() {
+    if (_pressedOrNot == "Button not pressed.") {
+      _pressedOrNot = "BUTTON PRESSED.";
+    } else {
+      _pressedOrNot = "Button not pressed.";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text(
+        _pressedOrNot,
+      )),
+      floatingActionButton: FloatingActionButton(onPressed: _changeText),
+    );
+  }
+}
