3d3d97fb0810efef8d99c25c1640314df954d5de
Elvis Madiba <38106494+not-diba@users.noreply.github.com>
2022-12-28 00:37:19 +0300

    Switches, Conditional Operators, const and final key words

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'playground_1.dart';
+// import 'playground_1.dart';
 
 main() => runApp(const Playground());
 
@@ -15,55 +15,68 @@ class Playground extends StatelessWidget {
         primarySwatch: Colors.orange,
       ),
       home: const SafeArea(
-        child: _MyHomePage(),
+        child: MyHomePage(),
       ),
     );
   }
 }
 
-class _MyHomePage extends StatelessWidget {
-  const _MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+const _youAre = "You are";
+const _compatible = "compatible with \nDorris D. Developer.";
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _ageSwitchValue = false;
+  String _messageToUser = "$_youAre NOT $_compatible";
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.grey[400],
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: buildColumn(context),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Are you compatible with Doris?"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            _buildAgeSwitch(),
+            _buildResultArea(),
+          ],
+        ),
       ),
     );
   }
-}
 
-Widget buildTitleText() {
-  return Container(
-    margin: const EdgeInsets.only(top: 10.0),
-    child: const Text(
-      "My Playground",
-      textScaleFactor: 3.0,
-      textAlign: TextAlign.center,
-    ),
-  );
-}
+  Widget _buildAgeSwitch() {
+    return Row(
+      children: <Widget>[
+        const Text("Are you 18 or Older ?"),
+        Switch(
+          value: _ageSwitchValue,
+          onChanged: _updateAgeSwitch,
+        )
+      ],
+    );
+  }
 
-Widget buildRoundedBox(
-  String label, {
-  double height = 88.0,
-}) {
-  return Container(
-    height: height,
-    width: 88.0,
-    alignment: const Alignment(0.0, 0.0),
-    decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: const Color.fromRGBO(255, 87, 51, 1.0)),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(20.0),
-        )),
-    child: Text(
-      label,
+  Widget _buildResultArea() {
+    return Text(
+      _messageToUser,
       textAlign: TextAlign.center,
-    ),
-  );
+    );
+  }
+
+  void _updateAgeSwitch(bool x) {
+    setState(() {
+      _ageSwitchValue = x;
+      _messageToUser =
+          _youAre + (_ageSwitchValue ? " " : " NOT ") + _compatible;
+    });
+  }
 }
