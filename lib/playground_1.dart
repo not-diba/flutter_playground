6dab79e16b4c88bfaec39b28530ffe437e47aa28
Elvis Madiba <38106494+not-diba@users.noreply.github.com>
2022-12-27 22:53:25 +0300

    Nested Columns and Rows

--- lib/playground_1.dart
+++ lib/playground_1.dart
@@ -9,9 +9,41 @@ Widget buildColumn(BuildContext context) {
     children: <Widget>[
       buildTitleText(),
       const SizedBox(height: 20.0),
-      buildRoundedBox(
-        "Sale Today",
-        height: 150.0,
+      _buildCaptionedRow(),
+    ],
+  );
+}
+
+Widget _buildCaptionedRow() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      _buildCaptionedItem(
+        "Cat",
+        caption: "Meow",
+      ),
+      _buildCaptionedItem(
+        "Dog",
+        caption: "Woof",
+      ),
+      _buildCaptionedItem(
+        "Ape",
+        caption: "Chatter",
+      ),
+    ],
+  );
+}
+
+Widget _buildCaptionedItem(String label, {required String caption}) {
+  return Column(
+    children: <Widget>[
+      buildRoundedBox(label),
+      const SizedBox(
+        height: 5.0,
+      ),
+      Text(
+        caption,
+        textScaleFactor: 1.25,
       )
     ],
   );
