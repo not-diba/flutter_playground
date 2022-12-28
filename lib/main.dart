29a495f12c9d340ea7a7a5267130f7fcbf921e2f
Elvis Madiba <38106494+not-diba@users.noreply.github.com>
2022-12-28 23:35:29 +0300

    Navigation Using a movie app, pushing and popping pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// import 'playground_1.dart';
+import 'playground_1.dart';
 
 main() => runApp(const Playground());
 
@@ -14,107 +14,60 @@ class Playground extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: const SafeArea(
-        child: MyHomePage(),
+      home: SafeArea(
+        child: MovieTitlePage(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
+class MovieTitlePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MovieTitlePageState createState() => MovieTitlePageState();
 }
 
-const _youAre = "You are";
-const _compatible = "compatible with \nDorris D. Developer.";
+class MovieTitlePageState extends State<MovieTitlePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Movie Title',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: buildTitlePageCore(),
+        ),
+      ),
+    );
+  }
+}
 
-class _MyHomePageState extends State<MyHomePage> {
-  bool _ageSwitchValue = false;
-  String _messageToUser = "$_youAre NOT $_compatible";
+class DetailPage extends StatelessWidget {
+  final overview = '(From themoviedb.com) One day at work, unsuccessful '
+      'puppeteer Craig finds a portal into the head of actor John '
+      'Malkovich. The portal soon becomes a passion for anybody who '
+      'enters its mad and controlling world of overtaking another human '
+      'body.';
+
+  const DetailPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Are you compatible with Doris?"),
+        title: const Text(
+          'Details',
+        ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            _buildAgeSwitch(),
-            _buildResultArea(),
-          ],
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: buildDetailPageCore(context),
         ),
       ),
     );
   }
-
-  Widget _buildAgeSwitch() {
-    return Row(
-      children: <Widget>[
-        const Text("Are you 18 or Older ?"),
-        Switch(
-          value: _ageSwitchValue,
-          onChanged: _updateAgeSwitch,
-        )
-      ],
-    );
-  }
-
-  Widget _buildResultArea() {
-    double loveFlutterSliderValue = 1.0;
-
-    return Column(
-      children: <Widget>[
-        ElevatedButton(
-          onPressed: _updateResults,
-          child: const Text("Submit"),
-        ),
-        const SizedBox(width: 15.0),
-        const Text(
-            "On a scale of 1 to 10, how much do you love flutter apps ?"),
-        Slider(
-          min: 1.0,
-          max: 10.0,
-          divisions: 9,
-          value: loveFlutterSliderValue,
-          onChanged: _updateLoveFlutterSlider,
-          label: "${loveFlutterSliderValue.toInt()}",
-        ),
-        Text(
-          _messageToUser,
-          textAlign: TextAlign.center,
-        )
-      ],
-    );
-  }
-
-  void _updateLoveFlutterSlider(double x) {
-    setState(() {
-      loveFlutterSliderValue = x;
-    });
-  }
-
-  void _updateResults() {
-    setState(() {
-      _messageToUser =
-          _youAre + (loveFlutterSliderValue >= 8 ? " " : " NOT ") + _compatible;
-    });
-  }
-
-  void _updateAgeSwitch(bool x) {
-    setState(() {
-      _ageSwitchValue = x;
-    });
-  }
-
-  // void _updateResults() {
-  //   setState(() {
-  //     _messageToUser = "You are${_ageSwitchValue ? " " : " NOT "}$_compatible";
-  //   });
-  // }
 }
--- lib/playground_1.dart
+++ lib/playground_1.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/material.dart';
 
+import 'main.dart';
+
+extension MoreMovieTitlePage on MovieTitlePageState {
+  goToDetailPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const DetailPage(),
+      ),
+    );
+  }
+
+  Widget buildTitlePageCore() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        const Text(
+          'Being John Malkovich',
+          textScaleFactor: 1.5,
+        ),
+        const SizedBox(height: 16.0),
+        ElevatedButton.icon(
+          icon: const Icon(Icons.arrow_forward),
+          label: const Text('Details'),
+          onPressed: goToDetailPage,
+        ),
+      ],
+    );
+  }
+}
+
+extension MoreDetailPage on DetailPage {
+  Widget buildDetailPageCore(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          overview,
+        ),
+      ],
+    );
+  }
+}
